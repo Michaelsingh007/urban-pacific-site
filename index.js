438509a88e71e088d8ac1d8f96c3170ef175f485
Michaelsingh007 <ms@michaelsingh.co.uk>
2025-04-20 17:51:40 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold text-center">Urban Pacific Real Estate Ltd</h1>
+    </div>
+  );
+}
